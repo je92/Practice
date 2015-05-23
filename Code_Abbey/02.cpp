1e558082c7f3696e31b2c15b82e396eef5bba78c
je92 <jescutia@mail.csuchico.edu>
2015-05-23 14:01:38 -0700

    Added problem 02

---
+++ Code_Abbey/02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/* Code Abbey Problem #2
+
+Input data has the following format:
+  <>N - amount of values to sum
+  <>User then inputs N number of values
+*/
+
+int main()
+{
+    int sum = 0;
+
+    int N; //where N is the size of input
+    cin >> N;
+
+    for(int i = 0; i < N; i++)
+    {
+        int x; //x is our user input to add to the sum
+        cin >> x;
+        sum += x;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
