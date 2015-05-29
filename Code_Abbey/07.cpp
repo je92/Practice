e6897229a34b5bda0a77f85b6bc19e8ad076ab52
je92 <jescutia@mail.csuchico.edu>
2015-05-29 16:29:16 -0700

    Added 07.cpp

---
+++ Code_Abbey/07.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //f to c formula (F-32)/1.8
+    int n;
+    cin >> n;
+
+    int f, c; //fahrenhit and celcius variables
+    double x; //temporary variable used for division and rounding to int
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> f;
+        x = (f-32)/1.8;
+
+        if( x > 0 )
+        {
+            c = x + 0.5;
+            cout << c << " ";
+        }
+        else
+        {
+            c = x - 0.5;
+            cout << c << " ";
+        }
+    }
+    return 0;
+}
